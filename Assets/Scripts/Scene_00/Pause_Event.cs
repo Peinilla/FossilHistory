725353646f4daa73f1f006490d1b56aa15a4b23e
godiacbear <telesisks@naver.com>
2018-07-10 14:02:33 +0900

    1시정지

---
+++ Assets/Scripts/Scene_00/Pause_Event.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Event : MonoBehaviour {
+    private bool pauseOn = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ButtonClick()
+    {
+        if (!pauseOn)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+        pauseOn = !pauseOn;
+    }
+}
