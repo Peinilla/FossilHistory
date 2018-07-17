fc698b3e3a153273e0b81b1b524f78cbafeda20f
godiacbear <telesisks@naver.com>
2018-07-17 16:12:34 +0900

    Merge branch 'master' of https://github.com/Peinilla/FossilHistory

--- Assets/Scripts/Game_Scripts/Lotus_Move.cs
+++ Assets/Scripts/Game_Scripts/Lotus_Move.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lotus_Move : MonoBehaviour {
+public class Lotus_Move : MonoBehaviour
+{
+    GameObject player;
+    public Vector3 startpos;
+    public float velocity = 2.56f;
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 pos = this.gameObject.transform.position;
+        startpos = pos;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("Player");
+        float movement = velocity * Time.deltaTime;
+        Vector3 nowpos = this.gameObject.transform.position;
+        float tr = nowpos.x - startpos.x;
+        transform.Translate(new Vector3(movement, 0, 0));
+        if (tr >= 5.12f && velocity >= 0.00f)
+        {
+            velocity = -velocity;
+        }
+        else if (tr <= -5.12f && velocity <= 0.00f)
+        {
+            velocity = -velocity;
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-
+    }
 }
